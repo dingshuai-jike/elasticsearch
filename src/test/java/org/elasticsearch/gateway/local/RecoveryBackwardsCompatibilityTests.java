0ece46f630fa57523684c1e4dc2bed386640704c
Simon Willnauer <simonw@apache.org>
2014-07-08 12:35:04 +0200

    moved test into different package

--- src/test/java/org/elasticsearch/gateway/local/RecoveryBackwardsCompatibilityTests.java
+++ src/test/java/org/elasticsearch/gateway/local/RecoveryBackwardsCompatibilityTests.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.elasticsearch.bwcompat;
+package org.elasticsearch.gateway.local;
 
 import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.Version;
